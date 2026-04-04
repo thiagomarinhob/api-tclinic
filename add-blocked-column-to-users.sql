e6b56b31256affadc730c732d781c77024d26513
thiagomarinhob <thiagomarinhobezerra@gmail.com>
2026-04-04 15:39:54 -0300

    remove files

--- add-blocked-column-to-users.sql
+++
@@ -1,5 +0,0 @@
--- Script para adicionar a coluna blocked na tabela users
--- Este script adiciona a coluna blocked para controle de acesso dos usuários
--- Execute este script no banco de dados PostgreSQL
-
-ALTER TABLE users ADD COLUMN IF NOT EXISTS blocked BOOLEAN NOT NULL DEFAULT false;
--- fix-birth-date-nullable.sql
+++
@@ -1,5 +0,0 @@
--- Script para corrigir a coluna birth_date na tabela users
--- Este script permite que a coluna birth_date aceite valores NULL
--- Execute este script no banco de dados PostgreSQL
-
-ALTER TABLE users ALTER COLUMN birth_date DROP NOT NULL;
